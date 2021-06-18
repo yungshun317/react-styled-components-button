a5ed3d04d6aedcdbf0b6b59a20baf92043cb5528
yungshun317 <seraph317@gmail.com>
2021-06-18 14:02:35 +0800

    [CHORE] Initialize a React application and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, "src", "index.js")
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        sourceMapFilename: "bundle.map"
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+        open: "google-chrome"
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        },{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html")
+        })
+    ]
+}
