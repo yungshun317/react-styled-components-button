675d341c38118b884ec3d38c27b9b46e8deec6fb
yungshun317 <seraph317@gmail.com>
2021-06-18 14:09:05 +0800

    [CHORE] Install packages

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,6 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
-        },{
             test: /\.js$/,
             exclude: /node_modules/,
             use: ["babel-loader"]
