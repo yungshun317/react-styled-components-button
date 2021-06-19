69f57c3321e0c4cea61faa01388ea52588dc3675
yungshun317 <seraph317@gmail.com>
2021-06-19 16:52:16 +0800

    [FEAT] Attach additional props to the rendered component

--- src/Button.js
+++ src/Button.js
@@ -3,13 +3,21 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-    color: #FFFFFF;
-    border: 2px solid palevioletred;
-    background-color: #DB8270;
+    margin: 1rem;
+    padding: 0.25rem 1rem;
+    font-size: 1rem;
+    border-radius: 3px; 
+
+    color: ${props => props.primary ? "#4FC0E8" : "#FFFFFF"};
+    border: 1px solid ${props => props.primary ? "palevioletred" : "#FFFFFF"};
+    background-color: ${props => props.primary ? "#DB8270" : "#000000"};
 `;
 
 export default function Button() {
     return (
-        <StyledButton>Button</StyledButton>
+    	<>
+            <StyledButton>Button</StyledButton>
+            <StyledButton primary>Primary Button</StyledButton>
+        </>
     );
 }
