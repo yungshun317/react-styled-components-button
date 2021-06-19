3d48959e4bf822f1328393106d9b9273fb13046c
yungshun317 <seraph317@gmail.com>
2021-06-19 17:07:58 +0800

    [FEAT] Use the `theme` prop and the `ThemeProvider` wrapper component

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 
 const StyledButton = styled.button`
     margin: 1rem;
@@ -8,16 +8,38 @@ const StyledButton = styled.button`
     font-size: 1rem;
     border-radius: 3px; 
 
+    // [2] Theming
+    color: ${props => props.theme.main};
+    border: 1px solid ${props => props.theme.main};
+    background-color: transparent;
+
+    /* [1] Attach additional props
     color: ${props => props.primary ? "#4FC0E8" : "#FFFFFF"};
     border: 1px solid ${props => props.primary ? "palevioletred" : "#FFFFFF"};
     background-color: ${props => props.primary ? "#DB8270" : "#000000"};
+    */
 `;
+
+// Default theme
+StyledButton.defaultProps = {
+	theme: {
+		main: "palevioletred"
+	}
+}
+
+// Define `props.theme``
+const theme = {
+	main: "mediumseagreen"
+}
 
 export default function Button() {
     return (
     	<>
             <StyledButton>Button</StyledButton>
-            <StyledButton primary>Primary Button</StyledButton>
+
+            <ThemeProvider theme={theme}>
+                <StyledButton>Themed Button</StyledButton>
+            </ThemeProvider>
         </>
     );
 }
